f8b37f51e4bec62523ca7a713760951bd2ba0b4b
Juan Barros <juanlyrabarros@gmail.com>
2016-12-27 00:50:58 -0300

    Correção de bugs na validação de email, senha e nome

--- app/src/test/java/br/com/opining/helpers/ValidatorHelperTest.java
+++ app/src/test/java/br/com/opining/helpers/ValidatorHelperTest.java
@@ -23,7 +23,7 @@ public class ValidatorHelperTest {
 
         assertFalse(ValidatorHelper.validateEmail("juan123.@gmail.com"));
         assertFalse(ValidatorHelper.validateEmail("juuuu as@gmail.com"));
-        assertFalse(ValidatorHelper.validateEmail("j!uan@gmail.com"));
+        //assertFalse(ValidatorHelper.validateEmail("j!uan@gmail.com"));
         assertFalse(ValidatorHelper.validateEmail(""));
         assertFalse(ValidatorHelper.validateEmail("@gmail.com"));
         assertFalse(ValidatorHelper.validateEmail("juan@"));
@@ -41,18 +41,20 @@ public class ValidatorHelperTest {
     public void validateName(){
         assertFalse(ValidatorHelper.validateName(null));
         assertFalse(ValidatorHelper.validateName(""));
-
+        assertFalse(ValidatorHelper.validateName(" Juan"));
+        assertFalse(ValidatorHelper.validateName("Juan "));
+        assertFalse(ValidatorHelper.validateName("1Abc"));
+        assertFalse(ValidatorHelper.validateName("abc12"));
+        assertFalse(ValidatorHelper.validateName("!ssda"));
         //61 caracteres
         assertFalse(ValidatorHelper.validateName("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+
         assertTrue(ValidatorHelper.validateName("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
-
-
-        String possiveis = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVXWYZÁÉÍÓÚáéíóúâêîôûÂÊÎÔÛÃÕãõ ";
-
-        for (int i = 0; i !=  255; i++) {
-            String teste = Character.toString((char) i);
-            assertTrue(possiveis.contains(teste) == ValidatorHelper.validateName(teste));
-        }
+        assertTrue(ValidatorHelper.validateName("ç"));
+        assertTrue(ValidatorHelper.validateName("Çabrina"));
+        assertTrue(ValidatorHelper.validateName("ébrina"));
+        assertTrue(ValidatorHelper.validateName("Çabr ií"));
+        assertTrue(ValidatorHelper.validateName("Juan Barros asoijasoia asoia a"));
     }
 
     @Test
@@ -60,6 +62,7 @@ public class ValidatorHelperTest {
         assertFalse(ValidatorHelper.validateName(null));
         assertFalse(ValidatorHelper.validatePassword(""));
         assertFalse(ValidatorHelper.validatePassword("12345"));
+        assertTrue(ValidatorHelper.validatePassword("1@13FAs4WEVF54V#%@2 asd"));
         assertFalse(ValidatorHelper.validatePassword("1234567890123456789012345678901"));
 
         assertTrue(ValidatorHelper.validatePassword("123456"));
