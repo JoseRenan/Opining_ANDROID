435623fdc28a4b05f8af835081e58bee364b6a67
José Renan <joserenansl99@gmail.com>
2016-12-27 21:48:21 -0300

    Criado snackbar verde para aviso de sucesso

--- app/src/main/java/br/com/opining/mvp/register/RegisterActivity.java
+++ app/src/main/java/br/com/opining/mvp/register/RegisterActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -70,7 +69,7 @@ public class RegisterActivity extends AppCompatActivity implements RegisterView 
     @Override
     public void notifyRegisterError(String msg) {
         startLoading(false);
-        AndroidHelper.showSnackbar(this, msg);
+        AndroidHelper.showErrorSnackbar(this, msg);
     }
 
     @Override
