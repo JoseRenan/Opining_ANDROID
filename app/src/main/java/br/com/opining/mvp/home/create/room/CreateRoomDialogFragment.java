673c02c0c252f09cadfd9fa825c1d4c95efa2acb
Juan Barros <juanlyrabarros@gmail.com>
2017-01-04 00:59:59 -0300

    Layout melhorado do dialog de criação

--- app/src/main/java/br/com/opining/mvp/home/create/room/CreateRoomDialogFragment.java
+++ app/src/main/java/br/com/opining/mvp/home/create/room/CreateRoomDialogFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import br.com.opining.R;
@@ -26,19 +27,25 @@ public class CreateRoomDialogFragment extends DialogFragment implements CreateRo
 
         View view = inflater.inflate(R.layout.modal_new_room, null);
         edtPost = (EditText) view.findViewById(R.id.edt_post);
+        Button btn_create_room = (Button) view.findViewById(R.id.btn_create_room);
 
-        builder.setView(view)
-                .setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        createRoom();
-                    }
-                }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        CreateRoomDialogFragment.this.getDialog().cancel();
-                    }
-                });
+        btn_create_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createRoom();
+            }
+        });
 
+        Button btn_cancel = (Button) view.findViewById(R.id.btn_cancel);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        builder.setView(view);
         return builder.create();
     }
 
